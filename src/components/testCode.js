865fe42342320bec79a62c6af60934553600bf0d
chenghuijun <chenghuijun@meituan.com>
2021-12-30 19:58:33 +0800

    leetcode: 215 数组中的第K个最大元素

--- src/components/testCode.js
+++ src/components/testCode.js
@@ -1,36 +1,30 @@
-export default function() {
-  document.write(lengthOfLongestSubstring("jbpnbwwd"));
-}
-
-/**
- * @param {string} s
- * @return {number}
- */
- var lengthOfLongestSubstring = function(s) {
-  let noRepeatArray = []
-  let stringArray = s ? s.split('') : []
-  console.log('stringArray: ', stringArray);
-  let max = 0
-
-  for (var i=0; i < stringArray.length; i++) { 
-     for (var j=i; j < stringArray.length; j++) { 
-         let char = stringArray[j]
-         console.log('char: ', char);
-         console.log('noRepeatArray.indexOf(char) === -1: ', noRepeatArray.indexOf(char) === -1);
-         if (noRepeatArray.indexOf(char) === -1) {
-             noRepeatArray.push(char)
-         } else if (noRepeatArray.length > max) {
-             max = noRepeatArray.length;
-             noRepeatArray = []
-             break;
-         } else {
-             noRepeatArray = []
-            break;
-         }
-         console.log('noRepeatArray: ', noRepeatArray);
-
-     }
+var findKthLargest = function(nums, k) {
+  if(!nums.length) return -1
+  // 调堆
+  let heapSort = function(arr, start, end) {
+      let temp = arr[start]
+      for(let j=2*start+1; j<=end; j=2*j+1) {
+          if(j<end && arr[j] < arr[j+1]) j++
+          if(temp > arr[j]) break
+          arr[start] = arr[j]
+          start = j
+      }
+      arr[start] = temp
   }
-
-  return max
+  let len = nums.length
+  for(let i=Math.floor(len/2-1); i>=0; i--)
+      heapSort(nums, i, len-1)
+  for(let i=len-1; i>0; i--) {
+      let temp = nums[0]
+      nums[0] = nums[i]
+      nums[i] = temp
+      heapSort(nums, 0, i-1) 
+  }
+  return nums[len-k]
 };
+
+
+export default function () {
+  findKthLargest([3, 2, 3, 1, 2, 4, 5, 5, 6], 4)
+  console.log('findKthLargest([3, 2, 3, 1, 2, 4, 5, 5, 6], 4): ', findKthLargest([3, 2, 3, 1, 2, 4, 5, 5, 6], 4));
+}
