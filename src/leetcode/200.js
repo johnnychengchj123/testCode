fc7f5e783ef4fb7c163a7bc1c7fb086d6e0b0b19
chenghuijun <chenghuijun@meituan.com>
2022-01-27 11:14:33 +0800

    53.最大子序和

--- src/leetcode/200.js
+++ src/leetcode/200.js
@@ -1,3 +1,4 @@
+
 function dfs(grid, r, c) {
   let nr = grid.length;
   let nc = grid[0].length;
@@ -5,6 +6,7 @@ function dfs(grid, r, c) {
   if (r < 0 || c < 0 || r >= nr || c >= nc || grid[r][c] == '0') {
       return;
   }
+  // 深度优先遍历，遍历过的致为0
   grid[r][c] = '0';
   dfs(grid, r - 1, c);
   dfs(grid, r + 1, c);
@@ -16,7 +18,6 @@ function numIslands(grid) {
   if (grid == null || grid.length == 0) {
       return 0;
   }
-
   let nr = grid.length;
   let nc = grid[0].length;
   let num_islands = 0;
